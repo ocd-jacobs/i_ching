44cdbebee1098d3a0cb59570679c5de6c02c964e
John Jacobs <jjacobs@xs4all.nl>
2014-09-26 02:36:47 +0200

    Add header comments to i_ching.pl

--- i_ching.pl
+++ i_ching.pl
@@ -1,6 +1,30 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
-# Yarrow stalks simulator
+# *******************************************************************************
+# File   : i_ching.pl
+# ------------------------------------------------------------------------------
+# Author : John Jacobs <jjacobs@xs4all.nl>
+# Date   : 26 september 2014
+# Version: 0.1
+#
+# Description: Perl script for consulting the I-Ching
+#
+# Remarks: original Yarrow stalks simulator code by TGI
+#
+# (C) 2014: This program is free software: you can redistribute it and/or modify
+#           it under the terms of the GNU General Public License as published by
+#           the Free Software Foundation, either version 3 of the License, or
+#           (at your option) any later version.
+#
+#           This program is distributed in the hope that it will be useful,
+#           but WITHOUT ANY WARRANTY; without even the implied warranty of
+#           MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#           GNU General Public License for more details.
+#
+#           You should have received a copy of the GNU General Public License
+#           along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# *******************************************************************************
+
 use 5.010;
 use strict;
 use warnings;
